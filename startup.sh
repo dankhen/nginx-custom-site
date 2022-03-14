776b3a1fe90a2c66050e38525cda7c69cf8ee8f7
Danny Khen <dankhen@amazon.com>
2022-03-14 10:45:21 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # this is a list of all variables supported
 if [ "${INDEX_HTML_CONTENT}" ]; then echo $INDEX_HTML_CONTENT > /usr/share/nginx/html/index.html; \
-                                     echo "<style> body {background-color: red;} </style>" >> /usr/share/nginx/html/index.html; \
+                                fi
+if [ "${INDEX_HTML_BG_COLOR}" ]; then echo "<style> body {background-color: $INDEX_HTML_BG_COLOR;} </style>" >> /usr/share/nginx/html/index.html; \
                                 fi
 if [ "${HTTP_PORT}" ]; then sed -i "s/listen       80/listen       ${HTTP_PORT}/g" /etc/nginx/conf.d/default.conf; fi
 # the following is required in some rare circumstances where a volume is mounted at /var/log (ref: https://github.com/knative/serving/issues/3809)  
